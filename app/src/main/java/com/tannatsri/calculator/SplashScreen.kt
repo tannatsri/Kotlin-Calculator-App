63080d5315746e6252040e60b75033706d1f9843
Mac <tanishqedu@gmail.com>
2021-12-11 00:19:29 +0530

    Initial commit of calculator app

---
+++ app/src/main/java/com/tannatsri/calculator/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.tannatsri.calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+//        Handler().postDelayed()
+    }
+
+}
